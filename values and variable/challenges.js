8ff352603617e80c72eaff5ae42e90689c6cf2da
Vikash975814 <118985043+Vikash975814@users.noreply.github.com>
2023-02-28 02:32:48 +0530

    Create challenges.js

---
+++ values and variable/challenges.js
@@ -0,0 +1,8 @@
+console.log(10+"20");
+ console.log(10-"5");
+ console.log("java "+"script");
+ console.log("Rahul"-"Raju");
+ console.log(true+true);
+ console.log(true+false);
+ console.log(false-true);
+ console.log(true-false);
