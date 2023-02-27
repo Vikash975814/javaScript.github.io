8845b824066246b7a16e474773919e75a8db9c95
Vikash975814 <118985043+Vikash975814@users.noreply.github.com>
2023-02-28 02:30:33 +0530

    Create values.js

---
+++ values and variable/values.js
@@ -0,0 +1,11 @@
+  var myName="Vikash" ;
+  console.log(myName);
+  console.log(typeof(myName));
+
+ var iamVikas=true;
+ console.log(iamVikas);
+ console.log(typeof(iamVikas));
+
+
+ var myAge=21 ;
+ console.log(myAge);
