1cfc12eb98956e152df140e1d0b283f6538a1954
Vikash975814 <118985043+Vikash975814@users.noreply.github.com>
2023-02-28 02:33:56 +0530

    Create NaN.js

---
+++ values and variable/NaN.js
@@ -0,0 +1,10 @@
+var myphoneNumber=8623232362;
+var myName="vikas";
+console.log(isNaN(myphoneNumber));
+console.log(isNaN(myName));
+console.log(NaN==NaN);
+console.log(Number.NaN==NaN);
+console.log(isNaN(Number.NaN));
+console.log(isNaN(NaN));
+console.log(Number.isNaN(NaN));
+
