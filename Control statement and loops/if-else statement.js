9761274176d2ecdf76f2763dc97dcfdb4a06e6e6
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-01 15:49:28 +0530

    Create if-else statement.js

---
+++ Control statement and loops/if-else statement.js
@@ -0,0 +1,44 @@
+var tomr="rain";
+if(tomr="rain"){
+    console.log("take a rain coat");
+}else{
+    console.log("No need to take a rain coat");
+}
+
+var age=20;
+if(age>18){
+    console.log("you are eligible to vote");
+}else{
+    console.log("you are not eligible to vote");
+}
+
+if(x=0){
+    console.log("print true statement");
+}else{
+    console.log("print wrong statement");
+}
+
+if(""){
+    console.log("print true statement");
+}else{
+    console.log("print wrong statement");
+}
+
+var x;
+if(x){
+    console.log("print true statement");
+}else{
+    console.log("print wrong statement");
+}
+
+if(NaN){
+    console.log("print true statement");
+}else{
+    console.log("print wrong statement");
+}
+
+if(null){
+    console.log("print true statement");
+}else{
+    console.log("print wrong statement");
+}
