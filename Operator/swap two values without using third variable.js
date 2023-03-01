d951cec8a32fbf3a543527c3bd0a00d3bea62490
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-01 15:43:37 +0530

    Create swap two values without using third variable.js

---
+++ Operator/swap two values without using third variable.js
@@ -0,0 +1,12 @@
+// swap two values without using third variable
+ var a=15;
+ var b=10;
+ console.log("Before values");
+ console.log("a is: "+a);
+ console.log("b is: "+b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ console.log("After swap values");
+ console.log("a is: "+a);
+ console.log("b is: "+b);
