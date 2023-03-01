3524618001c7ca81e133dae157fd1273bd87b5a3
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-01 15:37:25 +0530

    Create Increament or decreament.js

---
+++ Operator/Increament or decreament.js
@@ -0,0 +1,19 @@
+ var x=5;
+ var num=x++;
+ console.log("Num is:"+num);
+console.log("x is:"+x);
+
+var x=5;
+ var num=++x;
+ console.log("Num is:"+num);
+ console.log("x is:"+x);
+
+var x=5;
+ var num=x--;
+  console.log("Num is:"+num);
+  console.log("x is:"+x);
+ 
+ var x=5;
+ var num=--x;
+ console.log("Num is:"+num);
+ console.log("x is:"+x);
