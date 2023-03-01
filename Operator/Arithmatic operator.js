3661e230093c185d1976bf8f5a06f4b5a45116a1
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-01 15:35:44 +0530

    Create Arithmatic operator.js

---
+++ Operator/Arithmatic operator.js
@@ -0,0 +1,5 @@
+ console.log("Add is:"  +(3+3));
+ console.log("Subtract is:"  +(13-3));
+ console.log("Mul is:"  +(3*3));
+ console.log("Division is:"  +(10/2));
+ console.log("Remainder is:"  +(13%4));
